bb0f47e0abfd92d4be9b6ad30449438913ffc94b
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-03-27 11:54:10 -0500

    Updates

---
+++ precache-manifest.12decdf505c7dee68d1c04cfc6563c38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c0522f1347ce77d25f3",
+    "url": "/React-Seasons/static/js/runtime~main.6471c894.js"
+  },
+  {
+    "revision": "3ea9aed2f4f1639fa5b9",
+    "url": "/React-Seasons/static/js/main.1091a368.chunk.js"
+  },
+  {
+    "revision": "0e6ca656114bb3c77457",
+    "url": "/React-Seasons/static/js/2.1b58a2ad.chunk.js"
+  },
+  {
+    "revision": "3ea9aed2f4f1639fa5b9",
+    "url": "/React-Seasons/static/css/main.d86afc54.chunk.css"
+  },
+  {
+    "revision": "6d2920f498d250392f1f8215d84efb58",
+    "url": "/React-Seasons/index.html"
+  }
+];
